792a4170ea0e757a46498afde29482d9ebfac85e
Markus Reiß <e1126188@student.tuwien.ac.at>
2016-12-02 15:55:42 +0100

    Aufgabe 1.2: halbfertig

--- ue1/aufgabe1.m
+++ ue1/aufgabe1.m
@@ -84,3 +84,80 @@ end
 clear all;close all;clc;
 
 % Aufgabe 1.2.1
+
+%m s(2) + c s(1)  + k s = f
+
+%x1 = s
+%x2 = s(1)
+
+%x1. = x2 
+%x2. = 1/m * (f - c*x2 - k+x1)
+
+%[x1., x2.] = [[0, 1], [-k/m, -c/m]][x1, x2] + [0, 1/m]*u
+%y = [1,0][x1, x2]
+
+syms c  k  m f x1 x2
+
+% Anfangswerte
+s0 = 0;
+v0 = 0;
+x0 = [s0; v0];
+
+% Zustandsvektor, Eingangsvektor
+x = [x1; x2];
+u = f;
+
+% Systemmatrizen
+A = [0 1; -k/m -c/m];
+B = [0;1/m];
+C = [1,0];
+D = 0;
+
+xx = A*x + B*u;
+y = C*x + D*u;
+
+% Eigenwerte
+lambda = eig(A)
+
+% Eigenewrte l1,2 = -(c +/- (c^2 - 4*k*m)^(1/2))/(2*m)
+% dann ist der realteil fix negativ (-c/2m)
+lambda
+l1 = lambda(1); % = -0.5 + 1.3229 i = -0.5 + sqrt(-7)/2
+l2 = lambda(2); % = -0.5 - 1.3229 i = -0.5 - sqrt(-7)/2
+
+% Realteile der Eigenwerte sind negativ, deswegen ist das System stabil.
+real(lambda)
+
+% Transitionsmatrix; expm = e^Matrix
+t = sym('t');
+phi = expm(A * t)
+
+% Einheitssprung Sigma(t) - wir betrachten nur t >= 0
+sig = 1;
+f = sig;
+u = f;
+
+% Parameter setzen
+c = 1; 
+m = 1;
+k = 2;
+Ta = 0.05;
+
+% Kontiunierliches System generieren
+A = [0 1; -k/m -c/m];
+B = [0;1/m];
+C = [1,0];
+D = 0;
+sys = ss(A,B,C,D)
+
+
+
+
+
+
+sa = dsolve('D2s = -(2/1)*s - (1/1)*Ds +1')
+
+sp = dsolve('D2s = -(2/1)*s - (1/1)*Ds + 1','s(0)=0', 'Ds(0)=0')
+
+
+
