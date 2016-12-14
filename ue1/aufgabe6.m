288cfbb0c81ff2f93524c86fb59bf1256e20e3be
flozzone <flozzone@gmail.com>
2016-12-14 15:36:11 +0100

    1.6: Behebe Intergrationsfehler und cleanup.

--- ue1/aufgabe6.m
+++ ue1/aufgabe6.m
@@ -7,7 +7,7 @@ clear all;close all;clc;
 
 %% Aufgabe 1.6.1 - Zustandsraumdarstellung des Prozessors
 
-syms dc dck dp dpk c T1 s Ta
+syms dc dck dp dpk c T1 s Ta Tau
 syms A b ct d G_s Phi_s Gamma_s Phi_p Gamma_p
 
 % G(s) = dc_s / dp_s = (c)/(s*T1 + 1) = (c/t1)*(1/(s + 1/T1))
@@ -15,58 +15,56 @@ syms A b ct d G_s Phi_s Gamma_s Phi_p Gamma_p
 % G(s) = c'*(sE -A)^(-1)*b + d
 
 % Phi_p = (sE -A)^(-1) = 1/(s + 1/T1)
-% Koeffizientenvergleich
+% Koeffizientenvergleich, Bzw. ueber die 2. std. Normalform
 % => s - A = s + 1/t1 => A = -1/T1
-% => c' = c/T1 , b = 1 , d = 0
+% => c' = 1 , b = c/T1 , d = 0
 
 A = -1/T1;
-b = c;
+b = c/T1;
 ct = 1;
 d = 0;
 Phi_s = (s-A)^(-1);
 
 G_s = simplify(ct*Phi_s*b + d)
 
-% Kontinuierliches System
+
+% Kontinuierliches System, 2. std. Normalform des Prozessors
 % mit u = dp und y = dc
 dc_ = A*dc + b*dp
 y   = ct*dc + d*dp
 
 % Abtastsystem dazu 
-% wie auf Seite 145 auf Satz 2.4 angewandt
+% wie auf Seite 145 auf ueber Satz 2.4 hergeleitet, Gleichung 6.19a,b
 % Gamma_p = int (0, Ta) [exp(A*t) dt] = Ta(1 - e^(-Ta/T1))
 
 Phi_p = exp(A*Ta);
-Phi_p_ = diff (Phi_p, Ta);
-Gamma_p = (1 - Phi_p)*b;
+Gamma_p = int(exp(A*Tau), Tau, 0, Ta)*b
 
-dck_ = Phi_p*dck + Gamma_p*dpk
+
+dck_1 = Phi_p*dck + Gamma_p*dpk
 y_p = ct*dck
 
-%simplify(subs(dck_, [Ta, T1, c], [1*10^(-9), 1*10^(-3), 0.5]));
-
-%% Aufgabe 1.6.2
+%% Aufgabe 1.6.2 - Gesamtsystem
 
 % uk = din - dout = dpk
 % dpk = a*Sk
 % yk = S
 syms a Sk uk din dout
-% Zustandsgroessen fuer das Gesamtsystem: xg = [Sk, dck]
+% Zustandsgroessen fuer das Gesamtsystem: x_g = [Sk, dck]
 % Sk+1 = aktueller Inhalt + ("Zufluesse" - "Abfluesse") * Abtastzeit [Da 
 % die Zufluesse und Abluesse als Raten angegeben sind]
-% Sk+1 = Sk + Ta*uk + Ta*dck - Ta*a*Sk
-% Sk+1 = Sk + Ta*(uk + dck - dpk);
-% dck+1 = ct*xk+1
-% dck+1 = ct*(Phi_p * xk + Gamma_p*dpk)
+% Sk+1 = Sk + Ta*uk + Ta*dck - Ta*dpk
+% Sk+1 = Sk + Ta*(uk + dck - a*Sk);
+% dck+1 = Phi_p * xk + Gamma_p*dpk
 dpk = a*Sk;
 
 % Gesamtsystemgleichungen
-Sk_  = (1 - a*Ta)*Sk + Ta*(uk + dck)
-dck_ = Phi_p*dck + Gamma_p*dpk
+Sk_1  = (1 - a*Ta)*Sk + Ta*(uk + dck)
+dck_1 = Phi_p*dck + Gamma_p*dpk
 
 % Matrizen und Zustandsvektor der Zustandsraumdarstellung fuer 
 % die Zustaende Sk und dck; _g steht fuer Gesamt
-x_g= [Sk dck]
+x_g= [Sk; dck]
 
 A_g = [(1-Ta*a) Ta; (1-exp(-Ta/T1))*a*c exp(-Ta/T1)]
 B_g = [Ta ; 0];
@@ -84,8 +82,8 @@ a = 0.4; %s^-1
 Phi_p_v = double(subs(Phi_p))
 Gamma_p_v = double(subs(Gamma_p))
 ct_v = double(subs(ct))
-A_g = double(subs(A_g))
-B_g = double(subs(B_g))
+A_g_v = double(subs(A_g))
+B_g_v = double(subs(B_g))
 
 % Diskrete Zustandsdarstellung fuer den Processor
 sys_p = ss(Phi_p_v, Gamma_p_v, ct_v, d, Ta)
