fb79fcaf3ce577103df379b0229b316c5b06acad
Markus Reiß <e1126188@student.tuwien.ac.at>
2016-12-27 21:31:00 +0100

    Aufgabe 2.2

--- ue2/aufgabe_2.m
+++ ue2/aufgabe_2.m
@@ -53,14 +53,21 @@ f_M_r = subs(d_x_M, [i_GSM   phi_GSMP   w_GSM   w_P   u_GSM   M_ext], ...
                       [i_GSM_r phi_GSMP_r w_GSM_r w_P_r u_GSM_r M_ext_r]);
 
 % TODO                  
-i_GSM_r = solve(f_M_r(1), i_GSM_r);
-w_GMS_r = simplify(subs(solve(f_M_r(3), w_GSM_r)));
-w_P_r = solve(subs(f_M_r(4), w_P_r));
+%i_GSM_r = solve(f_M_r(1), i_GSM_r);
+%w_GMS_r = simplify(subs(solve(f_M_r(3), w_GSM_r)));
+%w_P_r = solve(subs(f_M_r(4), w_P_r));
 
 
 % Systemmatrizen des linearisierten Systems
 A = [diff(f_M, i_GSM) diff(f_M, phi_GSMP) diff(f_M, w_GSM) diff(f_M, w_P)];
+A = simplify(subs(A, w_P, w_P_r));
 bu = [diff(f_M, u_GSM)];
 bd = [diff(f_M, M_ext)];
 ct = [0 0 0 1];
+
+% Eigenwerte der Systemmatrix A berechnen
+paralist_1 = [u_GSM_r M_ext_r L_GSM R_GSM k_GSM J_GSM   d_cGSM d_vGSM J_P     d_cP  d_vP   d_qP c_GSMP d_GSMP];
+paralist_2 = [5.6     0       1.4   0.46  0.1   12.4e-3 0.152  1.8e-3 32.5e-3 0.169 2.7e-3 1e-4 0.6822 1e-5];
+
+A_num = simplify(subs(A, paralist_1, paralist_2))
 
