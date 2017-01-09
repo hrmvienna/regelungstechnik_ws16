d53b72e70c7ffcd14eeb726b5c900f5ebd2e24e4
Markus Reiß <e1126188@student.tuwien.ac.at>
2017-01-10 00:44:25 +0100

    Aufgabe 2.5: Reglerentwurf

--- ue2/aufgabe_5.m
+++ ue2/aufgabe_5.m
@@ -14,8 +14,8 @@ syms c_GSMP d_GSMP phi_GSMP
 Ta = 50e-03;
 
 % Parameterliste
-paralist_1 = [u_GSM_r M_ext_r L_GSM R_GSM k_GSM J_GSM   d_cGSM d_vGSM J_P     d_cP  d_vP   d_qP c_GSMP d_GSMP];
-paralist_2 = [5.6     0       1.4   0.46  0.1   12.4e-3 0.152  1.8e-3 32.5e-3 0.169 2.7e-3 1e-4 0.6822 1e-5];
+paralist_1 = [u_GSM_r M_ext_r L_GSM    R_GSM k_GSM J_GSM   d_cGSM d_vGSM J_P     d_cP  d_vP   d_qP c_GSMP d_GSMP];
+paralist_2 = [5.6     0       1.4e-3   0.46  0.1   12.4e-3 0.152  1.8e-3 32.5e-3 0.169 2.7e-3 1e-4 0.6822 1e-5];
 
 % TODO: evtl anpassen, falls sich Aufgabe 2.3 aendert
 % Linearisiertes System aus Aufgabe 2.3
@@ -52,6 +52,7 @@ Gq = tf_q(1)
 Gqd = tf_q(2)
 
 %% Reglerentwurf
+%% A: Streckenuebertragungsfunktion
 % Sollsprung delta_r = 20 rad s^-1
 delta_r = 20;
 
@@ -65,16 +66,43 @@ u_e = 0;
 u_gsm_min = 0;
 u_gsm_max = 12;
 
-% Quadratischer Term des Reglers aus den konj.compl. Polstellen errechnen
+
+%% B: Kenngroessen berechnen: Durchtrittsfrequenz und Phasenreserve
+
+% Quadratischer Term der Strecke aus den konj.compl. Polstellen errechnen
 pole_sys = pole(sys_q);
 p_con1 = pole_sys(1);
 p_con2 = pole_sys(2);
 syms x
 coeff_p = coeffs(simplify(((x - p_con1))*(x - (p_con2))), x);
-xi2T = coeff_p(2);
-Tsq  = coeff_p(1);
+ac = coeff_p(2);
+bc = coeff_p(1);
+xi2T = ac/bc;
+Tsq  = 1/bc;
 T = double(sqrt(Tsq))
 xi = double(xi2T /(2*T))
 
+% Kenngroessen berechnen
+omega_c = 1.2/t_r
+phi_soll = 70 - u_e
+
+% Da e_inf = 0 auf die Eingangsfolge (r^k) = (1^k), wird mind. ein
+% Integrator benoetigt.
+
+%% C: Reglerentwurf: zuerste Phase und Verstaerkung der bekannten Terme 
+
 % Regler aus den bekannten Termen
-Rq_1 = tf([(T^2) (2*xi*T) 1], [1 0], Ta)
+Rq_komp = tf([(T^2) (2*xi*T) 1], [1], Ta);
+Rq_1 = tf([1], [1 0], Ta)*Rq_komp
+Lq_1 = Rq_1*Gq;
+
+% Phasenreserve bei L_1(I*omega_c)
+[re_Lq_1 im_Lq_1] = nyquist(Lq_1, omega_c);
+phi_Lq_1 = atan (im_Lq_1/re_Lq_1) * 180/pi % phi = arctan(Im/re)[rad], [degree] = [rad]*180/pi,
+                                              % - 180 um in den richtigen
+                                              % Quadranten zu kommen.
+phi_dif = -180 + phi_soll - phi_Lq_1
+
+% Phase muss um -49.4686 gesenkt werden
+
+bode (Rq_1*Gq)
