0f2e7852764559b55642e41f597f5465a6bdec41
hrmvienna <hrm.vienna@gmail.com>
2017-01-09 12:15:29 +0100

    FKL

--- ue2/FKL.m
+++ ue2/FKL.m
@@ -10,10 +10,22 @@ ue=5;
 
 %% Aufgabe 2.1.2 PI Regler
 
+T = 1.002e-3;
+T_2 = 8.0863e-04;
+xi = 0.802;
+V = 1.377;
+V_G = V;
+V_R = 369.8044;
+
+b = [V_G];
+a = [(T^2) 2*xi*T 1];
+Gs = tf(b, a)
+
 %Gesamtregler und offener Kreis
-% R_PI = 
-% L_PI = 
-% bode(L_PI,'r');
+%R_PI = V_R*(T_2*s +1)/s;
+R_PI = V_R*tf([T_2 1], [1 0]);
+L_PI = R_PI*Gs;
+bode(L_PI,'r');
 
 %% Aufgabe 2.1.3 PID Regler
 
