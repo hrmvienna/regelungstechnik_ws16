ff9e1e04a9b2b8db49972b44aa17f43cb2b80db8
Florin Hillebrand <e0925917@student.tuwien.ac.at>
2017-01-10 20:04:56 +0100

    2.6: Versuch den Regler einzubauen.

--- ue2/aufgabe_6.m
+++ ue2/aufgabe_6.m
@@ -1,5 +1,57 @@
 clear all; close all; clc
 
+%% asd
+
+%  Systems im Arbeitspunkt aus Aufgabe 2.3
+
+% Zustandsgroessen
+syms delta_phi_GSMP delta_w_GSM delta_w_P w_P_r u_GSM_r M_ext_r
+% GSM
+syms L_GSM R_GSM k_GSM J_GSM d_cGSM d_vGSM u_GSM
+% P
+syms J_P d_cP d_vP d_qP
+% GSMP
+syms c_GSMP d_GSMP phi_GSMP
+Ta = 50e-03;
+
+% Parameterliste
+paralist_1 = [u_GSM_r M_ext_r L_GSM    R_GSM k_GSM J_GSM   d_cGSM d_vGSM J_P     d_cP  d_vP   d_qP c_GSMP d_GSMP];
+paralist_2 = [5.6     0       1.4e-3   0.46  0.1   12.4e-3 0.152  1.8e-3 32.5e-3 0.169 2.7e-3 1e-4 0.6822 1e-5];
+
+% TODO: evtl anpassen, falls sich Aufgabe 2.3 aendert
+% Linearisiertes System aus Aufgabe 2.3
+w_P_r = -(R_GSM*d_vP + R_GSM*d_vGSM - (k_GSM^4 + R_GSM^2*d_vP^2 + ...
+    R_GSM^2*d_vGSM^2 - 4*M_ext_r*R_GSM^2*d_qP - 4*R_GSM^2*d_cP*d_qP - ...
+    4*R_GSM^2*d_cGSM*d_qP + 2*R_GSM^2*d_vP*d_vGSM + 2*R_GSM*d_vP*k_GSM^2 + ...
+    2*R_GSM*d_vGSM*k_GSM^2 + 4*R_GSM*d_qP*k_GSM*u_GSM_r)^(1/2) + ...
+    k_GSM^2)/(2*R_GSM*d_qP);
+
+A = [             0,                                        1,                               -1;...
+ -c_GSMP/J_GSM, -(k_GSM^2/R_GSM + d_GSMP + d_vGSM)/J_GSM,                        d_GSMP/J_GSM;...
+    c_GSMP/J_P,                               d_GSMP/J_P, -(d_GSMP + d_vP + 2*d_qP*w_P_r)/J_P];
+bu = [0; k_GSM/(J_GSM*R_GSM); 0];
+bd = [0; 0; -1/J_P];
+ct = [0 0 1];
+d = 0;
+
+A = double(simplify(subs(A, paralist_1, paralist_2)));
+bu = double(simplify(subs(bu, paralist_1, paralist_2)));
+bd = double(simplify(subs(bd, paralist_1, paralist_2)));
+
+sys = ss(A, [bu, bd], ct, d);
+tf_sys = tf(sys);
+
+% Uebertragungsfunktionen T_ry und T_dy, wobei r der Eingang und d die
+% Stoerung ist
+Gu = tf_sys(1);
+Gd = tf_sys(2);
+
+% Zeitdiskrete Uebertragungsfunktion T_ry und T_dy
+Gz = c2d(Gu, Ta, 'zoh');
+Gzd = c2d(Gd, Ta, 'zoh');
+Gq = d2c(Gz, 'tustin');
+Gqd = d2c(Gzd, 'tustin');
+
 %% Systemmodelle aus Aufgabe 2.4.1
 
 syms u_GSM M_ext i_GSM phi_GSMP w_GSM w_P M_GSM M_kopp
@@ -96,5 +148,10 @@ d_red = d_lin;
 syms z
 
 Ta = 50e-03;
+Rz_num = [1,-2.71703075091639,2.62172975292077,-0.898592291920137];
+Rz_den = [1,-2.40425531914894,1.89723856948846,-0.492983250339520];
+Rz = tf(Rz_num, Rz_den, Ta);
 
-Rz = tf([1,-2.71703075091639,2.62172975292077,-0.898592291920137], [1,-2.40425531914894,1.89723856948846,-0.492983250339520], Ta);
+Rq_num = [0.161188125525027,0.355923201106770,12.7221764965311,8.70443482314179];
+Rq_den = [1,14,49,0];
+Rq = tf(Rq_num, Rq_den);
