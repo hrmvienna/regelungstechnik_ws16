d4d3f4169fe526da763095cf6bb98048ad4cdac0
Markus Reiß <e1126188@student.tuwien.ac.at>
2017-01-11 14:02:46 +0100

    Aufgabe 2.6: Ordnung

--- ue2/aufgabe_6.m
+++ ue2/aufgabe_6.m
@@ -1,8 +1,6 @@
 clear all; close all; clc
 
-%% asd
-
-%  Systems im Arbeitspunkt aus Aufgabe 2.3
+%% Parameter und Ruhelage
 
 % Zustandsgroessen
 syms delta_phi_GSMP delta_w_GSM delta_w_P w_P_r u_GSM_r M_ext_r
@@ -12,53 +10,14 @@ syms L_GSM R_GSM k_GSM J_GSM d_cGSM d_vGSM u_GSM
 syms J_P d_cP d_vP d_qP
 % GSMP
 syms c_GSMP d_GSMP phi_GSMP
-Ta = 50e-03;
-
-% Parameterliste
-paralist_1 = [u_GSM_r M_ext_r L_GSM    R_GSM k_GSM J_GSM   d_cGSM d_vGSM J_P     d_cP  d_vP   d_qP c_GSMP d_GSMP];
-paralist_2 = [5.6     0       1.4e-3   0.46  0.1   12.4e-3 0.152  1.8e-3 32.5e-3 0.169 2.7e-3 1e-4 0.6822 1e-5];
-
-% TODO: evtl anpassen, falls sich Aufgabe 2.3 aendert
-% Linearisiertes System aus Aufgabe 2.3
-w_P_r = -(R_GSM*d_vP + R_GSM*d_vGSM - (k_GSM^4 + R_GSM^2*d_vP^2 + ...
-    R_GSM^2*d_vGSM^2 - 4*M_ext_r*R_GSM^2*d_qP - 4*R_GSM^2*d_cP*d_qP - ...
-    4*R_GSM^2*d_cGSM*d_qP + 2*R_GSM^2*d_vP*d_vGSM + 2*R_GSM*d_vP*k_GSM^2 + ...
-    2*R_GSM*d_vGSM*k_GSM^2 + 4*R_GSM*d_qP*k_GSM*u_GSM_r)^(1/2) + ...
-    k_GSM^2)/(2*R_GSM*d_qP);
-
-A = [             0,                                        1,                               -1;...
- -c_GSMP/J_GSM, -(k_GSM^2/R_GSM + d_GSMP + d_vGSM)/J_GSM,                        d_GSMP/J_GSM;...
-    c_GSMP/J_P,                               d_GSMP/J_P, -(d_GSMP + d_vP + 2*d_qP*w_P_r)/J_P];
-bu = [0; k_GSM/(J_GSM*R_GSM); 0];
-bd = [0; 0; -1/J_P];
-ct = [0 0 1];
-d = 0;
-
-A = double(simplify(subs(A, paralist_1, paralist_2)));
-bu = double(simplify(subs(bu, paralist_1, paralist_2)));
-bd = double(simplify(subs(bd, paralist_1, paralist_2)));
-
-sys = ss(A, [bu, bd], ct, d);
-tf_sys = tf(sys);
-
-% Uebertragungsfunktionen T_ry und T_dy, wobei r der Eingang und d die
-% Stoerung ist
-Gu = tf_sys(1);
-Gd = tf_sys(2);
-
-% Zeitdiskrete Uebertragungsfunktion T_ry und T_dy
-Gz = c2d(Gu, Ta, 'zoh');
-Gzd = c2d(Gd, Ta, 'zoh');
-Gq = d2c(Gz, 'tustin');
-Gqd = d2c(Gzd, 'tustin');
-
-%% Systemmodelle aus Aufgabe 2.4.1
 
 syms u_GSM M_ext i_GSM phi_GSMP w_GSM w_P M_GSM M_kopp
 
-u = [u_GSM M_ext].'
-y1 = [i_GSM phi_GSMP w_GSM w_P].'
-y2 = [M_GSM M_kopp].'
+u = [u_GSM M_ext].';
+y1 = [i_GSM phi_GSMP w_GSM w_P].';
+y2 = [M_GSM M_kopp].';
+
+Ta = 50e-03;
 
 u_GSM_r = 5.6;
 M_ext_r = 0;
@@ -77,7 +36,6 @@ d_qP    = 1e-04;
 c_GSMP  = 0.6822;
 d_GSMP  = 1e-05;
 
-% TODO Angaben anpassen, falls sich was in Aufgabe 2.2/2.3 aendert
 % Ruhelagen
 i_GSM_r = (u_GSM_r + (k_GSM*(R_GSM*d_vP + R_GSM*d_vGSM - ... 
     (k_GSM^4 + R_GSM^2*d_vP^2 + R_GSM^2*d_vGSM^2 - ...
@@ -121,7 +79,30 @@ phi_GSMP_0 = phi_GSMP_r;
 w_GSM_0 = w_GSM_r;
 w_P_0 = w_P_r;
 
-% Linearisiertes Modell
+%% Reduzierte System
+A_red = [             0,                                        1,                               -1;...
+ -c_GSMP/J_GSM, -(k_GSM^2/R_GSM + d_GSMP + d_vGSM)/J_GSM,                        d_GSMP/J_GSM;...
+    c_GSMP/J_P,                               d_GSMP/J_P, -(d_GSMP + d_vP + 2*d_qP*w_P_r)/J_P];
+bu_red = [0; k_GSM/(J_GSM*R_GSM); 0];
+bd_red = [0; 0; -1/J_P];
+ct_red = [0 0 1];
+d_red = 0;
+
+sys_red = ss(A_red, [bu_red, bd_red], ct_red, d_red);
+tf_sys_red = tf(sys_red);
+
+% Uebertragungsfunktionen T_ry und T_dy, wobei r der Eingang und d die
+% Stoerung ist
+Gu_red = tf_sys_red(1);
+Gd_red = tf_sys_red(2);
+
+% Zeitdiskrete Uebertragungsfunktion T_ry und T_dy
+Gz_red = c2d(Gu_red, Ta, 'zoh');
+Gzd_red = c2d(Gd_red, Ta, 'zoh');
+Gq_red = d2c(Gz_red, 'tustin');
+Gqd_red = d2c(Gzd_red, 'tustin');
+
+%% Linearisiertes Modell
 A_lin = [ -R_GSM/L_GSM, 0, -k_GSM/L_GSM, 0; ...
     0, 0, 1, -1; ...
     k_GSM/J_GSM, -c_GSMP/J_GSM, -(d_GSMP + d_vGSM)/J_GSM, d_GSMP/J_GSM; ...
@@ -133,18 +114,22 @@ b_lin = [bu_lin bd_lin];
 c_lin = [0 0 0 1];
 d_lin = [0 0];
 
-% Reduziertes Modell
-A_red = [0,1,-1; ...
--c_GSMP/J_GSM, -(k_GSM^2/R_GSM + d_GSMP + d_vGSM)/J_GSM, d_GSMP/J_GSM; ...
-c_GSMP/J_P, d_GSMP/J_P, -(d_GSMP + d_vP + 2*d_qP*w_P_r_red)/J_P];
+% Uebertragungsfunktion lin. System
+Gs_lin = ss(A_lin, b_lin, c_lin, d_lin);
+tf_sys_lin = tf(Gs_lin);
 
-bu_red = [0; k_GSM/(J_GSM*R_GSM); 0];
-bd_red = [0; 0; -1/J_P];
-b_red = [bu_red bd_red];
-c_red = [0 0 1];
-d_red = d_lin;
+% Uebertragungsfunktionen T_ry und T_dy, wobei r der Eingang und d die
+% Stoerung ist
+Gu_lin = tf_sys_lin(1);
+Gd_lin = tf_sys_lin(2);
 
-%% Aufgabe 2.6.1
+% Zeitdiskrete Uebertragungsfunktion T_ry und T_dy
+Gz_lin = c2d(Gu_lin, Ta, 'zoh');
+Gzd_lin = c2d(Gd_lin, Ta, 'zoh');
+Gq_lin = d2c(Gz_lin, 'tustin');
+Gqd_lin = d2c(Gzd_lin, 'tustin');
+
+%% PI-Komp Regler aus Aufgabe 2.5
 syms z
 
 Ta = 50e-03;
@@ -165,6 +150,7 @@ tr = 5; % Start Rampe
 Usinus = 1; % sin amp
 we = 2; % sin omega
 
-figure
-step((Rq*Gq)/(1 + Rq*Gq))
-grid on
+% figure
+% step((Rq*Gq)/(1 + Rq*Gq))
+% grid on
+
