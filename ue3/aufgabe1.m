6ea51dbd9a2ac9a0fe46fb2d9c52a4cc308efedd
hrmvienna <hrm.vienna@gmail.com>
2017-01-17 20:58:51 +0100

    Aufgabe 3.1.1

--- ue3/aufgabe1.m
+++ ue3/aufgabe1.m
@@ -2,7 +2,32 @@ clear all; close all; clc
 
 %% Aufgabe 3.1.1: Zustandsreglerentwurf
 
-% TODO
+% Parameter
+Ta = 10e-03; % 10ms
+
+% Lin. red. Modell aus Aufgabe 2.3
+A = double([0, 1, -1; -3411/62, -54163/28520, 1/1240; ...
+    6822/325, 1/3250, 54117/74750 - (7*1130385^(1/2))/7475]);
+bu = double([0; 12500/713; 0]);
+bd = double([0; 0; -400/13]);
+ct = [0 0 1];
+d = 0;
+sys = ss(A,[bu, bd], ct, d)
+
+% System abtasten
+dsys = c2d(sys, Ta)
+
+phi = dsys.A;
+gamma = dsys.B(:,1); % Spalte 1
+
+% Erreichbarkeitsmatrix
+R = [gamma, phi*gamma, phi*phi*gamma];
+
+if rank(R) == 3
+   sprintf('System ist vollstaendig erreichbar')
+else
+    sprintf('System ist nicht vollstaendig erreichbar')
+end
 
 %% Aufgabe 3.1.2: diskreter Zustandsregler
 
