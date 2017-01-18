2f54a1cdd6acc7524808c48c5fc55e6bd2c3fcbe
hrmvienna <hrm.vienna@gmail.com>
2017-01-18 12:08:01 +0100

    Aufgabe 3.1

--- ue3/aufgabe1.m
+++ ue3/aufgabe1.m
@@ -21,7 +21,7 @@ phi = dsys.A;
 gamma = dsys.B(:,1); % Spalte 1
 
 % Erreichbarkeitsmatrix
-R = [gamma, phi*gamma, phi*phi*gamma];
+R = ctrb(phi, gamma);
 
 if rank(R) == 3
    sprintf('System ist vollstaendig erreichbar')
@@ -31,10 +31,52 @@ end
 
 %% Aufgabe 3.1.2: diskreter Zustandsregler
 
-syms delta_phi_GSMP delta_w_GSM delta_w_P delta_uk delta_rk
+syms phi_GSMP w_GSM w_P uk rk g
+kt = sym('kt', [1 3]);
 
-delta_x_red = [delta_phi_GSMP delta_w_GSM delta_w_P].';
+xk = [phi_GSMP w_GSM w_P].';
+uk = kt*xk + g*rk;
 
-% Polvorgabe
-P = [1, 2, 3];
-kt = acker(phi, gamma, P)
+dxk = phi*xk + gamma*uk;
+yk = ct*xk + d*uk;
+
+% Ueberpruefung, dxk == dxk2, Skript Kapitel 8.1
+phi_g = phi + gamma*kt;
+dxk2 = phi_g*xk + gamma*g*rk;
+
+% Eigenwerte der Dynamikmatrix
+eigen_phi = eig(phi);
+
+% Polvorgabe, 1/2 wurde gewaehlt, da es kleiner als die Eigenwerte von Phi
+% ist, d.h. schnellere Dynamik der gewaehlten Pole als von Phi
+lambda0 = -1/2;
+P = [exp(Ta*lambda0), exp(Ta*lambda0), exp(Ta*lambda0)];
+kt = -acker(phi, gamma, P) % Vorzeichen, Hinweis in Beispiel 8.1
+kt1 = kt(1); kt2 = kt(2); kt3 = kt(3);
+
+% Gleichung (8.39)
+g = 1 / ((ct + d*kt)*inv(eye(3) - phi - gamma*kt)*gamma + d)
+
+phi_g = double(subs(phi_g));
+% Streckenuebertragungsfunktion
+sys_g = ss(phi_g, gamma*g, (ct+ d*kt), d*g);
+Gz = tf(sys_g)
+
+figure
+bode(Gz, (Gz/(1+Gz)))
+
+%% Reglerentwurf
+% Anforderungen aus Aufgabe 2.5
+
+% Sollsprung delta_r = 20 rad s^-1
+delta_r = 20;
+
+% Bleibende Reglerabweichung fuer Eingangssprung r^k = (1^k) 
+e_inf_s = 0;
+% Antiegszeit
+t_r = 1;
+% Ueberschwingen
+u_e = 0;
+% Stellgroessenbeschraenkung
+u_gsm_min = 0;
+u_gsm_max = 12;
