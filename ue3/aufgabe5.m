ae8e5eff9522f0cba80a91dc9201f6dd81884bf2
Markus Reiß <e1126188@student.tuwien.ac.at>
2017-01-26 14:56:07 +0100

    Aufgabe 3.5

--- ue3/aufgabe5.m
+++ ue3/aufgabe5.m
@@ -4,6 +4,15 @@ clear all; close all; clc
 
 Ta = 0.01;
 
+% aus Aufgabe 3.2:
+A = double([0, 1, -1; -3411/62, -54163/28520, 1/1240; ...
+    6822/325, 1/3250, 54117/74750 - (7*1130385^(1/2))/7475]);
+bu = double([0; 12500/713; 0]);
+bd = double([0; 0; -400/13]);
+ct = [0 0 1]; d = 0;
+sys = ss(A,bu, ct, d);
+dsys = c2d(sys, Ta);
+
 Rz_komp_den = [1 14 49];
 Rz_komp_num = [0.0128407323859812 0.0194498756069027 1];
 Rz_komp = tf(Rz_komp_num, Rz_komp_den, Ta);
